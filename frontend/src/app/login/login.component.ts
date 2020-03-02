89d555ecd5341919eebfda6236df13a137a3326f
Adnan Lapendic <adnan.lapendic@gmail.com>
2020-03-02 19:48:24 +0100

    Add handle login method

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+username: string;
+password: string;
+errorMessage: string = "Invalid credentials";
+invalidLogin: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.username = "";
+    this.password = "";
   }
 
+  handleLogin(): void {
+   if(this.username === 'lapa' && this.password === '123') {
+     this.invalidLogin = false;
+   } else {
+     this.invalidLogin = true;
+   }
+
+
+     
+  }
 }
