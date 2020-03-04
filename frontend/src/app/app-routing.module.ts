b0fa75613797909763ecb8969bfa449c987424ee
Adnan Lapendic <adnan.lapendic@gmail.com>
2020-03-04 01:13:37 +0100

    Add logout component and implement logout funcionality

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
 const routes: Routes = [
   {path: '', component:LoginComponent},
   {path: 'login', component:LoginComponent},
+  {path: 'logout', component:LogoutComponent},
   {path: 'welcome/:name', component:WelcomeComponent},
   {path: 'todos',component: ListTodosComponent},
   {path: '**', component: ErrorComponent}
--- frontend/src/app/service/hardcoded-authentication.service.ts
+++ frontend/src/app/service/hardcoded-authentication.service.ts
@@ -7,11 +7,21 @@ export class HardcodedAuthenticationService {
 
   constructor() { }
 
-  authenticate(username: string, password: string){
+  authenticate(username: string, password: string): boolean{    
     if(username === 'lapa' && password === '123') {
+      sessionStorage.setItem('authenticatedUser', username)
       return true;
     } else {
       return false;
     }
+  }
+
+  isUserLogedIn(): boolean {
+    const user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
+  }
+
+  logout(): void {
+    sessionStorage.removeItem('authenticatedUser');
   }
 }
