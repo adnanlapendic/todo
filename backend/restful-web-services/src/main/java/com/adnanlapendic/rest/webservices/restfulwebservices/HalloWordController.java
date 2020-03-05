158e33c659703a9abf9d1000d590bf3588dbe7b6
Adnan Lapendic <adnan.lapendic@gmail.com>
2020-03-05 23:41:49 +0100

    Create test object and some test methods in controller

--- backend/restful-web-services/src/main/java/com/adnanlapendic/rest/webservices/restfulwebservices/HalloWordController.java
+++ backend/restful-web-services/src/main/java/com/adnanlapendic/rest/webservices/restfulwebservices/HalloWordController.java
@@ -1,6 +1,7 @@
 package com.adnanlapendic.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,15 @@ public class HalloWordController {
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean halloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean HelloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
---
+++ backend/restful-web-services/src/main/java/com/adnanlapendic/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.adnanlapendic.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+
+}
